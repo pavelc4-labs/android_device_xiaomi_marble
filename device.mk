4fae5032cae64407ccf62b74d4d4a876b6d9af43
Pavelc4 <pavelc4@users.noreply.github.com>
2026-03-28 23:54:28 +0700

    marble : Drop touch Feature
    Signed-off-by: Pavelc4 <pavelc4@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -407,12 +407,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal-service.qti
 
-# Touchscreen
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch-service.xiaomi
-
-$(call soong_config_set, XIAOMI_TOUCH, HIGH_TOUCH_POLLING_PATH, /sys/devices/virtual/touch/touch_dev/bump_sample_rate)
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
