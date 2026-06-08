49a1d383138f142bf67beeb779f0a5fab3c5112e
Pavelc4 <pavelc4@users.noreply.github.com>
2026-06-09 02:54:50 +0700

    marble : bring back dolby vision

--- extract-files.py
+++ extract-files.py
@@ -102,8 +102,6 @@ blob_fixups: blob_fixups_user_type = {
     ),
     'vendor/etc/sensors/hals.conf': blob_fixup()
         .add_line_if_missing('sensors.xiaomi.v2.so'),
-    'vendor/etc/vintf/manifest/c2_manifest_vendor.xml': blob_fixup()
-        .regex_replace('.+dolby.+\n', ''),
     (
         'vendor/lib64/hw/com.qti.chi.override.so',
         'vendor/lib64/libcamxcommonutils.so',
