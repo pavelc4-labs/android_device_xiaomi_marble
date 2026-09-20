6da62121096b0b588fe234aa95ebf3b9a10366d5
Arian <arian.kulmer@web.de>
2026-09-20 14:39:26 +0200

    marble: Drop WifiResTarget_spf
    All these are already set by WifiResCommon, which we also moved to vendor
    
    Change-Id: Id665d8efcbd32fc4ae1b1f6e00b5771d6b9a5968
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- device.mk
+++ device.mk
@@ -296,8 +296,7 @@ PRODUCT_PACKAGES += \
     WifiResMarbleCN
 
 PRODUCT_PACKAGES += \
-    FrameworksResTarget \
-    WifiResTarget_spf
+    FrameworksResTarget
 
 PRODUCT_PACKAGES += \
     FrameworksResXiaomi \
